99d90e05b0a3c3090457ee1e657116b3a655ae32
Konjointed <84739652+Konjointed@users.noreply.github.com>
2021-10-14 23:26:16 -0400

    mongodb

--- Main.py
+++ Main.py
@@ -1,19 +1,33 @@
 import discord
 import os
+import motor.motor_asyncio
 from discord.ext import commands
-from dotenv import load_dotenv
-from os import getenv
-load_dotenv()
+from utils.mongo import Document
+
 
 intents = discord.Intents.default()
 intents.members = True
 intents.reactions = True
 intents.messages = True
 intents.guilds = True
+
+async def GetPrefix(client,msg):
+    if not msg.guild:
+        return commands.when_mentioned_or("!")(client,msg)
+
+    try:
+        data = await client.config.find(msg.guild.id)
+
+        if not data or "prefix" not in data:
+             return commands.when_mentioned_or("!")(client,msg)
+        return commands.when_mentioned_or(data["prefix"])(client,msg)
+    except:
+        return commands.when_mentioned_or("!")(client,msg) 
+
 client = commands.Bot(
-    command_prefix = commands.when_mentioned_or("!"),
+    command_prefix = GetPrefix,
     case_insenitive = True,
-    status = discord.Status.idle,
+    status = discord.Status.idle, 
     intents = intents,
 )
 client.activity = discord.Game(name=f"Watching over {str(len(client.guilds))} server(s)")
@@ -34,5 +48,11 @@ async def on_ready():
     print(f"Logged in as {client.user}")
     print(f"Bot is in {str(len(client.guilds))} server(s)")
 
-#client.run(getenv("TOKEN"))
+    client.mongo = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGO"])
+    client.db = client.mongo["dbName"]
+    client.config = Document(client.db,"config")
+    print("Database Initalized")
+    for document in await client.config.get_all():
+        print(document)
+
 client.run(os.environ["TOKEN"])
--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 from typing import Optional
 from discord.ext.commands import command, has_permissions, bot_has_permissions
 from discord.ext.commands import Cog, Greedy
-from discord import Embed, Member
+from discord import Embed, Member, message
 
 class Moderation(commands.Cog):
 
@@ -37,6 +37,27 @@ class Moderation(commands.Cog):
             for target in targets:
                 await target.kick(reason = reason)
 
+    @commands.bot_has_permissions(administrator = True)
+    @commands.has_permissions(administrator = True)
+    @commands.command(
+        name = "changeprefix",
+        help = "change the prefix of the bot",
+        aliases = ["newprefix","cp","changep","cprefix","np"] #np = newprefix cp = changeprefix
+    )
+    async def ChangePrefix(self,ctx,*,prefix="!"):
+        await self.client.config.upsert({"_id": ctx.guild.id, "prefix": prefix})
+        await ctx.send(f"The prefix has been changed to {prefix}")
+
+    @commands.bot_has_permissions(administrator = True)
+    @commands.has_permissions(administrator = True)
+    @commands.command(
+        name = "resetprefix",
+        help = "reset the current prefix back to the default one",
+        aliases = ["rp","rprefix","removeprefix"] 
+    )
+    async def ResetPrefix(self,ctx):
+        await self.client.config.unset({"_id": ctx.guild.id, "prefix": 1})
+        await ctx.send("Prefix has been reset to default")
 
 def setup(client):
     client.add_cog(Moderation(client))
