c8d0183202bf540a874750791b2659d803e95cfc
Konjointed <84739652+Konjointed@users.noreply.github.com>
2021-10-14 23:54:23 -0400

    trying to remove __pycache__

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 import discord
 import os
 import motor.motor_asyncio
+import sys
 from discord.ext import commands
 from utils.mongo import Document
 
+sys.dont_write_bytecode = True
 
 intents = discord.Intents.default()
 intents.members = True
@@ -16,9 +18,9 @@ async def GetPrefix(client,msg):
         return commands.when_mentioned_or("!")(client,msg)
 
     try:
-        data = await client.config.find(msg.guild.id)
+        data = await client.config.find(msg.guild.id) #find the data of the server in the mongo database by the id of the server
 
-        if not data or "prefix" not in data:
+        if not data or "prefix" not in data: #if data is not found (so no data at all) or no prefix is in the data return default stuff
              return commands.when_mentioned_or("!")(client,msg)
         return commands.when_mentioned_or(data["prefix"])(client,msg)
     except:
@@ -37,7 +39,7 @@ for FileName in os.listdir("./cogs"):
     if FileName.endswith(".py"):
         client.load_extension(f"cogs.{FileName[:-3]}")
 
-#global check
+#global check (temporaily having this here for learning)
 #runs whenever a command does
 @client.check
 async def globally_block_dms(ctx):
@@ -48,11 +50,10 @@ async def on_ready():
     print(f"Logged in as {client.user}")
     print(f"Bot is in {str(len(client.guilds))} server(s)")
 
+    #setup mongodb
     client.mongo = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGO"])
     client.db = client.mongo["dbName"]
     client.config = Document(client.db,"config")
     print("Database Initalized")
-    for document in await client.config.get_all():
-        print(document)
 
 client.run(os.environ["TOKEN"])
